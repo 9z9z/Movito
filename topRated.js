9f21996c0af6184671bae8fe9d23f89a692bc5dc
9z9z <aboqht217@gmail.com>
2025-07-28 10:59:42 +0300

    Update topRated.js

--- topRated.js
+++ topRated.js
@@ -1,5 +1,1 @@
-import { fetchFromTMDB } from './utils.js';
-const container = document.getElementById('top-rated');
-fetchFromTMDB('/movie/top_rated').then(data => {
-  container.innerHTML = data.results.map(m => `<div class="card"><img src="https://image.tmdb.org/t/p/w200${m.poster_path}" /><h4>${m.title}</h4></div>`).join('');
-});
+import{fetchFromTMDB}from'./utils.js';const c=document.getElementById('top-rated-container');(async()=>{const d=await fetchFromTMDB('/movie/top_rated');c.innerHTML=d.results.map(m=>`<div class="card" onclick="location.href='html/movieDetail.html?id=${m.id}'"><img src="https://image.tmdb.org/t/p/w300${m.poster_path}" /><h4>${m.title}</h4></div>`).join('');})();
