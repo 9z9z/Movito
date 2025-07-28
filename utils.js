e5e7ddf128f2de5d60e619be57504969078c4f76
9z9z <aboqht217@gmail.com>
2025-07-28 10:49:04 +0300

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,5 +1,8 @@
 export const apiKey = '6b2dec73b6697866a50cdaef60ccffcb';
+
 export async function fetchFromTMDB(path) {
-  const res = await fetch(`https://api.themoviedb.org/3${path}?api_key=${apiKey}&language=ar`);
+  const url = `https://api.themoviedb.org/3${path}?api_key=${apiKey}&language=ar`;
+  const res = await fetch(url);
+  if (!res.ok) throw new Error('فشل في جلب البيانات من TMDB');
   return res.json();
 }
