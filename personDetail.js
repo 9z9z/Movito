5a9c60c606b40e7ad43fe02a297c68a4ead09a1d
9z9z <aboqht217@gmail.com>
2025-07-28 10:50:59 +0300

    Update personDetail.js

--- personDetail.js
+++ personDetail.js
@@ -1,13 +1,22 @@
 import { fetchFromTMDB } from './utils.js';
+
 const id = new URLSearchParams(location.search).get('id');
 const container = document.getElementById('person-detail');
 
-fetchFromTMDB(`/person/${id}?append_to_response=movie_credits,tv_credits`).then(data => {
-  const movies = data.movie_credits.cast.map(i => `<a href="movieDetail.html?id=${i.id}">${i.title}</a>`).join('، ');
-  const shows = data.tv_credits.cast.map(i => `<a href="TvShowsDetails.html?id=${i.id}">${i.name}</a>`).join('، ');
-  container.innerHTML = `
-    <h1>${data.name}</h1>
-    <p><strong>أشهر الأفلام:</strong> ${movies}</p>
-    <p><strong>أشهر المسلسلات:</strong> ${shows}</p>
-  `;
-});
+async function renderPersonDetail() {
+  try {
+    const data = await fetchFromTMDB(`/person/${id}?append_to_response=movie_credits,tv_credits`);
+    const movies = data.movie_credits.cast.map(i => `<a href="movieDetail.html?id=${i.id}">${i.title}</a>`).join('، ');
+    const shows = data.tv_credits.cast.map(i => `<a href="TvShowsDetails.html?id=${i.id}">${i.name}</a>`).join('، ');
+    container.innerHTML = `
+      <h1>${data.name}</h1>
+      <p><strong>أشهر الأفلام:</strong> ${movies}</p>
+      <p><strong>أشهر المسلسلات:</strong> ${shows}</p>
+    `;
+  } catch(err) {
+    container.innerHTML = '<p>خطأ في تحميل بيانات الشخص</p>';
+    console.error(err);
+  }
+}
+
+renderPersonDetail();
